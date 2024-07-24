e7952e0c8b420ad8c38eaf560f352bd6c59d475d
dkMaster3000 <kunzd007@gmail.com>
2024-07-24 10:04:45 +0200

    Challenge six: ZigzagConversion faster

--- src/main/java/main/ZigzagConversion.java
+++ src/main/java/main/ZigzagConversion.java
@@ -1,30 +1,33 @@
 package main;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Objects;
 
 public class ZigzagConversion {
     public String convert(String s, int numRows) {
-
         HashMap<Integer, String> map = new HashMap<>();
-        List<Integer> rows = new ArrayList<>();
+
+
+        int idx = 0;
+        int d = 1;
 
         for (int i = 0; i < s.length(); i++) {
-            if (i == 0) {
-                map.put(1, String.valueOf(s.charAt(0)));
-                rows.add(1);
+            if (map.containsKey(idx)) {
+                map.put(idx, map.get(idx) + s.charAt(i));
             } else {
-                int row = getRow(rows, numRows);
-
-                if (map.containsKey(row)) {
-                    map.put(row, map.get(row) + s.charAt(i));
-                } else {
-                    map.put(row, String.valueOf(s.charAt(i)));
-                }
+                map.put(idx, String.valueOf(s.charAt(i)));
             }
+
+            if (idx == numRows - 1) {
+                d = -1;
+            }
+
+            if (idx == 0) {
+                d = 1;
+            }
+
+            idx += d;
         }
+
 
         StringBuilder m = new StringBuilder();
 
@@ -33,31 +36,5 @@ public class ZigzagConversion {
         }
 
         return m.toString();
-    }
-
-    public int getRow(List<Integer> rows, int numRows) {
-        int newRowNumber;
-        if (rows.getLast() == 1) {
-            newRowNumber = numRows >= 2 ? 2 : 1;
-        } else if (Objects.equals(rows.getLast() - 1, rows.get(rows.size() - 2))) {
-
-            if (rows.getLast() + 1 > numRows) {
-                newRowNumber = rows.getLast() - 1;
-
-            } else {
-                newRowNumber = rows.getLast() + 1;
-
-            }
-        } else {
-            if (rows.getLast() - 1 < 1) {
-                newRowNumber = rows.getLast() + 1;
-
-            } else {
-                newRowNumber = rows.getLast() - 1;
-
-            }
-        }
-        rows.add(newRowNumber);
-        return newRowNumber;
     }
 }
